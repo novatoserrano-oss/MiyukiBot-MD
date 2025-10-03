f2ea87846871340d6b4470ac62acecb1099c2be4
OmarGranda <omargranda673@gmail.com>
2025-10-03 16:28:48 -0500

    Add files via upload

---
+++ plugins/info-ping.js
@@ -0,0 +1,62 @@
+import speed from 'performance-now'
+import { exec } from 'child_process'
+import moment from 'moment-timezone'
+
+let handler = async (m, { conn }) => {
+  let timestamp = speed();
+  let latensi = speed() - timestamp;
+
+  const start = new Date().getTime();
+  const { key } = await conn.sendMessage(m.chat, { text: `*🍂 Calculando ping...*` }, { quoted: m });
+  const end = new Date().getTime();
+  const latency = end - start;
+
+  const uptime = process.uptime();
+  const hours = Math.floor(uptime / 3600);
+  const minutes = Math.floor((uptime % 3600) / 60);
+  const secondsUp = Math.floor(uptime % 60);
+  const uptimeFormatted = `${hours}h ${minutes}m ${secondsUp}s`;
+
+  const usedRAM = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2);
+
+  const fechaHora = moment().tz('America/Lima').format('YYYY/MM/DD, h:mm A');
+
+  exec(`neofetch --stdout`, async (error, stdout) => {
+    let child = stdout.toString("utf-8");
+    let sysInfo = child.replace(/Memory:/, "Ram:");
+
+    let response =
+`╭━━━〔 🍄 𝙎𝙩𝙖𝙩𝙪𝙨 𝙋𝙞𝙣𝙜 🪴 〕━━⬣
+│ 🌷 *\`Ping:\`* ${latency} ms
+│ 🍁 *\`Latencia:\`* ${latensi.toFixed(4)} ms
+│ 🕸 *\`RAM usada:\`* ${usedRAM} MB
+│ 🍧 *\`Uptime:\`* ${uptimeFormatted}
+│ ⏰ *\`Fecha/Hora:\`* ${fechaHora}
+╰━━━〔 🪷 𝙍𝙞𝙣 𝙄𝙩𝙤𝙨𝙝𝙞 〕━━⬣
+\`\`\`
+${sysInfo.trim()}
+\`\`\``;
+
+    await conn.sendMessage(m.chat, {
+      text: response,
+      mentions: [m.sender],
+      contextInfo: {
+        externalAdReply: {
+          title: '🍄 Rɪɴ Iᴛᴏsʜɪ ᴍᴅ 🌹 | 🪾 ʙʏ ᴅᴠ.sʜᴀᴅᴏᴡ 🪴',
+          body: club,
+          thumbnailUrl: await (await fetch(icono)).buffer(),
+          sourceUrl: redes,
+          mediaType: 1,
+          renderLargerThumbnail: true
+        }
+      }
+    }, { quoted: m });
+  });
+}
+
+handler.help = ['ping', 'p']
+handler.tags = ['info']
+handler.command = ['ping', 'p']
+handler.register = true
+
+export default handler
