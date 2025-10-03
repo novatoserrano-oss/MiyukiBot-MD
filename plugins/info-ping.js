dfbaec28620a2177daed241ec84f0916d7a087c5
OmarGranda <omargranda673@gmail.com>
2025-10-03 16:59:44 -0500

    Actualizar info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -1,39 +1,32 @@
 import speed from 'performance-now'
 import { spawn, exec, execSync } from 'child_process'
 
- handler = async (m, { conn }) => {
-  let timestamp = speed();
-  let latensi = speed() - timestamp;
+let handler = async (m, { conn }) => {
+    const start = new Date().getTime();
+await m.react('🚀')
+    const { key } = await conn.sendMessage(m.chat, {text: `Cargando Datos 📡`}, {quoted: m});
+    const end = new Date().getTime();
 
-  const start = new Date().getTime();
-  const { key } = await conn.sendMessage(m.chat, { text: `*🍂 Calculando ping...*` }, { quoted: m });
-  const end = new Date().getTime();
-  const latency = end - start;
+    const latency = end - start;
+    const seconds = (latency / 1000).toFixed(1);
 
-  const uptime = process.uptime();
-  const hours = Math.floor(uptime / 3600);
-  const minutes = Math.floor((uptime % 3600) / 60);
-  const secondsUp = Math.floor(uptime % 60);
-  const uptimeFormatted = `${hours}h ${minutes}m ${secondsUp}s`;
+    const uptime = process.uptime(); // en segundos
+    const hours = Math.floor(uptime / 3600);
+    const minutes = Math.floor((uptime % 3600) / 60);
+    const secondsUp = Math.floor(uptime % 60);
+    const uptimeFormatted = `${hours}h ${minutes}m ${secondsUp}s`;
 
-  const usedRAM = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2);
+    const usedRAM = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2); // en MB
 
-  const fechaHora = moment().tz('America/Lima').format('YYYY/MM/DD, h:mm A');
-
-  exec(`neofetch --stdout`, async (error, stdout) => {
-    let child = stdout.toString("utf-8");
-    let sysInfo = child.replace(/Memory:/, "Ram:");
-
-        let response = `╭━━━〔 🍄 𝙎𝙩𝙖𝙩𝙪𝙨 𝙋𝙞𝙣𝙜 🪴 〕━━⬣
-│ 📌 *\`Ping:\`* ${latency} ms
-│ 🍁 *\`Latencia:\`* ${latensi.toFixed(4)} ms
-│ 🕸 *\`RAM usada:\`* ${usedRAM} MB
-│ 🍧 *\`Uptime:\`* ${uptimeFormatted}
-│ ⏰ *\`Fecha/Hora:\`* ${fechaHora}
-╰━━━〔 𝙈𝙞𝙮𝙪𝙠𝙞𝘽𝙤𝙩-𝙈𝘿 🌸 〕━━⬣
+    setTimeout(async () => {
+        let response = `╭━━━〔 *Info Bot* 〕━━⬣
+│ Hola 👋 @${m.sender.split('@')[0]}. 
+│ 📍 \`Ping:\` ${latency} ms
+│ 🎌 \`Uptime:\` [ ${uptimeFormatted} ]
+│ 💾 \`RAM usada:\` ${usedRAM} MB
+╰━〔 𝙈𝙞𝙮𝙪𝙠𝙞𝘽𝙤𝙩-𝙈𝘿 〕━⬣
 
  © 𝘗𝘰𝘸𝘦𝘳𝘦𝘥 𝘉𝘺 𝘖𝘮𝘢𝘳𝘎𝘳𝘢𝘯𝘥𝘢`;
-
         await m.react('✅')
         await conn.sendMessage(m.chat, { text: response, edit: key, mentions: [m.sender] }, { quoted: m });
     }, latency);
