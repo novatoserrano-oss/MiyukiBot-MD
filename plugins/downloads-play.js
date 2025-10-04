31e8953076472223e378b51d4edc2606e46d611b
OmarGranda <omargranda673@gmail.com>
2025-10-03 23:46:32 -0500

    Actualizar downloads-play.js

--- plugins/downloads-play.js
+++ plugins/downloads-play.js
@@ -33,7 +33,7 @@ await m.react('✔️')
 } else if (['play2', 'mp4'].includes(command)) {
 const video = await getVid(url)
 if (!video?.url) throw '⚠️ No se pudo obtener el video.'
-await conn.sendFile(m.chat, video.url, `${title}.mp4
+await conn.sendFile(m.chat, video.url, `${title}.mp4`, 
 await m.react('✔️')
 }} catch (e) {
 await m.react('✖️')
