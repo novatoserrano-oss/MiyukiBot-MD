e5ffbc5df5b235d75a9c1b31d19a866944125aec
OmarGranda <omargranda673@gmail.com>
2025-10-04 23:44:13 -0500

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,22 +1,22 @@
 import { exec } from 'child_process';
 
 let handler = async (m, { conn }) => {
-  m.reply(`🗿 Actualizando el bot...`);
+  m.reply(`📡 *Actualizando el bot...*`);
 
   exec('git pull', (err, stdout, stderr) => {
     if (err) {
-      conn.reply(m.chat, `${msm} Error: No se pudo realizar la actualización.\nRazón: ${err.message}`, m);
+      conn.reply(m.chat, `${msm} *Error: No se pudo realizar la actualización*.\nRazón: ${err.message}`, m);
       return;
     }
 
     if (stderr) {
-      console.warn('Advertencia durante la actualización:', stderr);
+      console.warn('*Advertencia durante la actualización:*', stderr);
     }
 
     if (stdout.includes('Already up to date.')) {
-      conn.reply(m.chat, `🕸️ El bot ya está actualizado.`, m);
+      conn.reply(m.chat, `🚀 *El bot ya está actualizado al última versión*.`, m);
     } else {
-      conn.reply(m.chat, `✔️ Actualización realizada con éxito.\n\n${stdout}`, m);
+      conn.reply(m.chat, `✅ *Actualización realizada con éxito*.\n\n${stdout}`, m);
     }
   });
 };
