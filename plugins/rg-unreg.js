03ace6d177dbd9ab21bc16f5c53d19420c1835e4
OmarGranda <omargranda673@gmail.com>
2025-10-04 22:16:34 -0500

    Actualizar rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -1,13 +1,48 @@
-let handler = async (m, { conn, text }) => {
+handler = async (m, { conn, text }) => {
+  let user = global.db.data.users[m.sender]
 
-let user = global.db.data.users[m.sender]
+  let nombre = user.name || 'Sin nombre 💭'
+  let edad = user.age || 'Desconocida 💫'
+  let pp
+  try {
+    pp = await conn.profilePictureUrl(m.sender, 'image')
+  } catch {
+    pp = icono
+  }
 
-user.registered = false
-return conn.reply(m.chat, `🌱 Tu registro fue borrado de mi base de datos.`, m)
+  user.registered = false
 
+  await conn.sendMessage(m.chat, {
+    text: `🌸・｡ﾟ✧｡・ﾟ✿・｡ﾟ✧｡・ﾟ🌸
+      🍰✨ 𝑅𝐸𝐺𝐼𝑆𝑇𝑅𝑂 𝐸𝐿𝐼𝑀𝐼𝑁𝐴𝐷𝑂 ✨🍰
+🌸・｡ﾟ✧｡・ﾟ✿・｡ﾟ✧｡・ﾟ🌸
+
+🐰💖 *Nombre anterior:* ${nombre}  
+🎀🎂 *Edad:* ${edad} años  
+
+🦋💌 Esperamos verte de nuevo muy pronto, ¡te extrañaremos!  
+
+━━━━━━━━━━━━━━━
+🌷 𝑬𝒔𝒄𝒓𝒊𝒃𝒆: *.reg Nombre Edad*  
+para volver a registrarte 💕
+━━━━━━━━━━━━━━━`,
+    mentions: [m.sender],
+    contextInfo: {
+      externalAdReply: {
+        title: `🌸 Registro eliminado con éxito ${emojis}`,
+        body: `✨ Usuario: ${nombre} • ${edad} años ✨`,
+        thumbnailUrl: pp,
+        mediaType: 1,
+        renderLargerThumbnail: true,
+        sourceUrl: pp
+      }
+    }
+  }, { quoted: m })
 }
+
 handler.help = ['unreg']
 handler.tags = ['rg']
 handler.command = ['unreg']
 handler.register = true
+
 export default handler
