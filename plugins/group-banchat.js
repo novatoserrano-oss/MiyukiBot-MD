f7d56f1aba3103ec17341d949490d42efbcda82b
the-27 <blackoficial2025@gmail.com>
2025-10-08 22:44:22 -0500

    🎃 𝙎𝙝𝙖𝙙𝙤𝙬𝘾𝙤𝙧𝙚 🎋

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -4,20 +4,20 @@ if (command === 'bot') {
 if (args.length === 0) {
 const estado = chat.isBanned ? '✗ Desactivado' : '✓ Activado'
 const info = `「✦」Un administrador puede activar o desactivar a *${botname}* utilizando:\n\n✐ _Activar_ » *${usedPrefix}bot enable*\n✐ _Desactivar_ » *${usedPrefix}bot disable*\n\n✧ Estado actual » *${estado}*`
-return conn.reply(m.chat, info, m, rcanal)
+return conn.reply(m.chat, info, m, rcanal);
 }
 if (args[0] === 'off') {
 if (chat.isBanned) {
-return conn.reply(m.chat, `《✦》${botname} ya estaba desactivado.`, m, canal)
+return conn.reply(m.chat, `《✦》${botname} ya estaba desactivado.`, m, rcanal);
 }
 chat.isBanned = true
 return conn.reply(m.chat, `❀ Has *desactivado* a ${botname}!`, m)
 } else if (args[0] === 'on') {
 if (!chat.isBanned) {
-return conn.reply(m.chat, `《✦》${botname} ya estaba activado.`, m, rcanal)
+return conn.reply(m.chat, `《✦》${botname} ya estaba activado.`, m, rcanal);
 }
 chat.isBanned = false
-return conn.reply(m.chat, `❀ Has *activado* a ${botname}!`, m, rcanal)
+return conn.reply(m.chat, `❀ Has *activado* a ${botname}!`, m, rcanal);
 }}}
 
 handler.help = ['bot']
