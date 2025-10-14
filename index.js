2b50d95a2e13500451901f5eee75f92a0723cf82
OmarGranda <omargranda673@gmail.com>
2025-10-13 20:09:52 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import fs, { readdirSync, statSync, unlinkSync, existsSync, mkdirSync, readFileS
 import yargs from 'yargs'
 import { spawn, execSync } from 'child_process'
 import lodash from 'lodash'
-import { MiyukiBotMD } from './plugins/sockets-serbot.js'
+import { miyukijadibot } from './plugins/sockets-serbot.js'
 import chalk from 'chalk'
 import syntaxerror from 'syntax-error'
 import { tmpdir } from 'os'
