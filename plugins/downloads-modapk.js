d10dc7362ce3ea0331400c3312d9a9019fad86d8
✞  𝙎𝙝𝙖𝙙𝙤𝙬`𝘾𝙤𝙧𝙚 <blackoficial2025@gmail.com>
2025-10-20 12:11:18 -0500

    Actualizar downloads-modapk.js

--- plugins/downloads-modapk.js
+++ plugins/downloads-modapk.js
@@ -15,7 +15,7 @@ await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m)
 if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
 return await conn.reply(m.chat, `ꕥ El archivo es demasiado pesado.`, m)
 }
-await conn.sendMessage(m.chat, { document: { url: data5.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null }, { quoted: m })
+//await conn.sendMessage(m.chat, { document: { url: data5.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null }, { quoted: m })
 await m.react('✔️')
 } catch (error) {
 await m.react('✖️')
