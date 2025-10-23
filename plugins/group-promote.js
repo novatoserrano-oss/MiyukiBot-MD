cefddc6fc740f85e664dba92cedc8f5da66d1b40
DV-ux-alt <lopezacaro09@gmail.com>
2025-10-22 22:09:17 -0500

    Actualizar group-promote.js

--- plugins/group-promote.js
+++ plugins/group-promote.js
@@ -15,7 +15,7 @@ conn.reply(m.chat, `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}rep
 
 handler.help = ['promote']
 handler.tags = ['grupo']
-handler.command = ['promote', 'promover']
+handler.command = ['promote', 'promover', 'darpene*']
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
