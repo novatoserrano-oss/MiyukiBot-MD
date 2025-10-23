9d93ccdb576dc2598a824fd2d6a8c44aaea4f5a5
OmarGranda <omargranda673@gmail.com>
2025-10-23 07:16:35 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 let { say } = cfonts
 console.log(chalk.magentaBright('\n❀ Iniciando...'))
-say('Bot-MD', {
+say('MiyukiBot-MD', {
 font: 'simple',
 align: 'left',
 gradient: ['green', 'white']
