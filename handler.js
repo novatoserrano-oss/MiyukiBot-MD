ec3a8833f5e36a52cdcc87976fc90a8594f26109
OmarGranda <omargranda673@gmail.com>
2025-10-24 06:29:50 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -31,7 +31,7 @@ try {
     if (!m) return
     if (!m.isGroup) {
         const text = (m.text || '').trim().toLowerCase()
-        if (text !== 'sc') return
+        if (text !== '') return
     }
 
 m.exp = 0
