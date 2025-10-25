250c58f189eb3af8c4aa1a277d6533983ad99072
OmarGranda <omargranda673@gmail.com>
2025-10-25 09:00:20 -0500

    Actualizar _validcommand.js

--- plugins/_validcommand.js
+++ plugins/_validcommand.js
@@ -5,7 +5,7 @@ export async function before(m, { conn }) {
 
   const usedPrefix = global.prefix.exec(m.text)[0];
   const command = m.text.slice(usedPrefix.length).trim().split(' ')[0].toLowerCase();
-/*
+
   const thumbRes = await fetch("https://files.catbox.moe/3nmafy.jpg");
   const thumbBuffer = await thumbRes.buffer();
 
